fa043181ad4c253701df45c1fc81ab6fed010008
dani <45732406+danikoskas97@users.noreply.github.com>
2020-03-31 18:05:29 +0200

    Create HM.cs

---
+++ targil_cita_dani/HM.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace lesson03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          
+          
+          
+          
+        }
+    }
+}
